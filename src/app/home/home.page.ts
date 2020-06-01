1b956b211850eabd7ea20385a95fd995f106d839
ArturoCA271 <arturo.cardona107@gmail.com>
2020-06-01 18:56:15 -0500

    actualizacion con ruta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component,ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 declare var google;
 
 @Component({
@@ -12,20 +14,54 @@ declare var google;
 
 
 export class HomePage implements OnInit, AfterViewInit {
-  map;
-  @ViewChild('mapElement') mapElement: ElementRef;
-
-  constructor() {
+  @ViewChild('mapElement') mapNativeElement: ElementRef;
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionForm: FormGroup;
+  currentLocation: any = {
+    lat: 0,
+    lng: 0
+  };
+  constructor(private fb: FormBuilder, private geolocation: Geolocation) {
+    this.createDirectionForm();
   }
 
-  ngOnInit():void{ 
+  ngOnInit() {
+  }
+
+  createDirectionForm() {
+    this.directionForm = this.fb.group({
+      destination: ['', Validators.required]
+    });
   }
 
   ngAfterViewInit(): void {
-    const map = new google.maps.Map(this.mapElement.nativeElement, {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.currentLocation.lat = resp.coords.latitude;
+      this.currentLocation.lng = resp.coords.longitude;
+    });
+    const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    this.directionsDisplay.setMap(map);
+  }
+
+  calculateAndDisplayRoute(formValues) {
+    const that = this;
+    this.directionsService.route({
+      origin: this.currentLocation,
+      destination: formValues.destination,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        that.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
     });
   }
+
+
 
 }
